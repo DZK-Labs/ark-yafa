bcf730dc67eaac17cd5af610f2bfd7904c9bf478
孙如绿叶 <778398626@qq.com>
2022-09-02 14:21:45 +0800

    add test

--- src/yafa_146/curves/tests.rs
+++ src/yafa_146/curves/tests.rs
@@ -3,10 +3,11 @@
 use ark_algebra_test_templates::{
     curves::{curve_tests, sw_tests},
     generate_bilinearity_test, generate_g1_test, generate_g2_test,
+    generate_product_of_pairings_test,
     msm::*,
 };
 use ark_ec::{AffineCurve, PairingEngine, ProjectiveCurve};
-use ark_ff::{Field, MontFp, One, PrimeField};
+use ark_ff::{Field, MontFp, One, PrimeField, UniformRand};
 use ark_std::{rand::Rng, test_rng};
 use core::ops::MulAssign;
 
@@ -15,3 +16,4 @@ use crate::yafa_146::*;
 generate_g1_test!(yafa; curve_tests; sw_tests;);
 generate_g2_test!(yafa; curve_tests; sw_tests;);
 generate_bilinearity_test!(Yafa, Fq12);
+generate_product_of_pairings_test!(Yafa);
